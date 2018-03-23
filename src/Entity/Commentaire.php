56806fc3d09fcaad2627ab2f8a53cd448f99aab3
comtegan <student6@d3c.re>
2018-03-23 12:24:41 +0000

    Add entity Urgence Commentaire

---
+++ src/Entity/Commentaire.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentaireRepository")
+ */
+class Commentaire
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+
+    private $id_commentaire;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contenu_com;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getIdCommentaire(): ?int
+    {
+        return $this->id_commentaire;
+    }
+
+    public function setIdCommentaire(int $id_commentaire): self
+    {
+        $this->id_commentaire = $id_commentaire;
+
+        return $this;
+    }
+
+    public function getContenuCom(): ?string
+    {
+        return $this->contenu_com;
+    }
+
+    public function setContenuCom(string $contenu_com): self
+    {
+        $this->contenu_com = $contenu_com;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Urgence.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UrgenceRepository")
+ */
+class Urgence
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+
+    private $id_urgence;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $libelle_urgence;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getIdUrgence(): ?int
+    {
+        return $this->id_urgence;
+    }
+
+    public function setIdUrgence(int $id_urgence): self
+    {
+        $this->id_urgence = $id_urgence;
+
+        return $this;
+    }
+
+    public function getLibelleUrgence(): ?string
+    {
+        return $this->libelle_urgence;
+    }
+
+    public function setLibelleUrgence(?string $libelle_urgence): self
+    {
+        $this->libelle_urgence = $libelle_urgence;
+
+        return $this;
+    }
+}
