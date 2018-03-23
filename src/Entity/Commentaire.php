81c236858400e3728b452f21ca6ac43af2d236e2
comtegan <student6@d3c.re>
2018-03-23 12:39:36 +0000

    Add entity Urgence Commentaire

--- src/Entity/Commentaire.php
+++ src/Entity/Commentaire.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\CommentaireRepository")
+ * @ORM\Entity(repositoryClass="App\Repoitory\CommentaireRepository")
  */
 class Commentaire
 {
